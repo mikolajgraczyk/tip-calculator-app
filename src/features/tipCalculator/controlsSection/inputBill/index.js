0e54be2370ed7a03593d09d24d137c3dd7ad7722
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-03-16 18:55:02 +0100

    inputs are now singe source of truth

--- src/features/tipCalculator/controlsSection/inputBill/index.js
+++ src/features/tipCalculator/controlsSection/inputBill/index.js
@@ -1,18 +1,22 @@
-import {
-  StyledInputBill,
-  InputWrapper,
-  Input,
-} from "./styled";
+import { useState } from "react";
+import { StyledInputBill, InputWrapper, Input } from "./styled";
 import Title from "../title";
 import { ReactComponent as DollarIcon } from "./images/DollarIcon.svg";
 
 const InputBill = () => {
+  const [billAmount, setBillAmount] = useState("");
+
   return (
     <StyledInputBill>
       <Title title="Bill" />
       <InputWrapper>
         <DollarIcon />
-        <Input type="number" dir="rtl"></Input>
+        <Input
+          type="number"
+          dir="rtl"
+          value={billAmount}
+          onChange={({ target }) => setBillAmount(target.value)}
+        ></Input>
       </InputWrapper>
     </StyledInputBill>
   );
--- src/features/tipCalculator/controlsSection/inputPeople/index.js
+++ src/features/tipCalculator/controlsSection/inputPeople/index.js
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import { StyledInputPeople, InputWrapper, Input } from "./styled";
 import Title from "../title";
 import { ReactComponent as PersonIcon } from "./images/PersonIcon.svg";
 
 const InputPeople = () => {
+  const [peopleAmount, setPeopleAmount] = useState("");
+
   return (
     <StyledInputPeople>
       <Title title="Number of People" />
       <InputWrapper>
         <PersonIcon />
-        <Input type="number" dir="rtl"></Input>
+        <Input
+          type="number"
+          dir="rtl"
+          value={peopleAmount}
+          onChange={({ target }) => setPeopleAmount(target.value)}
+        ></Input>
       </InputWrapper>
     </StyledInputPeople>
   );
