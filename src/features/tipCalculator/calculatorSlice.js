55ea2fae8082f48b35f04cb39ea6d0d04ca35ef5
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-03-16 20:16:55 +0100

    fixed infinity tip issue

--- src/features/tipCalculator/calculatorSlice.js
+++ src/features/tipCalculator/calculatorSlice.js
@@ -17,28 +17,44 @@ const calculatorSlice = createSlice({
     insertPeopleAmount(state, { payload }) {
       state.peopleAmount = payload;
     },
-    resetData(state){
-        state.billAmount = "";
-        state.tipSelected = "";
-        state.peopleAmount = "";
+    resetData(state) {
+      state.billAmount = "";
+      state.tipSelected = "";
+      state.peopleAmount = "";
     },
   },
 });
 
-export const { insertBill, selectTip, insertPeopleAmount, resetData } = calculatorSlice.actions;
+export const { insertBill, selectTip, insertPeopleAmount, resetData } =
+  calculatorSlice.actions;
 
 export const selectCalculatorState = (state) => state.calculator;
 
-export const selectBillAmount = (state) => selectCalculatorState(state).billAmount;
-export const selectPeopleAmount = (state) => selectCalculatorState(state).peopleAmount;
-export const selectSelectedTip = (state) => selectCalculatorState(state).tipSelected;
+export const selectBillAmount = (state) =>
+  selectCalculatorState(state).billAmount;
+export const selectPeopleAmount = (state) =>
+  selectCalculatorState(state).peopleAmount;
+export const selectSelectedTip = (state) =>
+  selectCalculatorState(state).tipSelected;
 
 export const selectCalculatedTipAmount = (state) => {
-    return selectSelectedTip(state)/100 * selectBillAmount(state);
+  if (selectBillAmount(state) < 0) {
+    return 0.00;
+  }
+  return (selectSelectedTip(state) / 100) * selectBillAmount(state);;
 };
 
 export const selectTipAmountPerPerson = (state) => {
-    return selectCalculatedTipAmount(state)/selectPeopleAmount(state);
+  switch (true) {
+    case selectPeopleAmount(state) >= 1:
+      return selectCalculatedTipAmount(state) / selectPeopleAmount(state);
+    case selectPeopleAmount(state) === "":
+      return selectCalculatedTipAmount(state) / 1;
+    case selectPeopleAmount(state) < 0:
+      return 0.0;
+    default:
+      return 0.0;
+  }
 };
 
 export default calculatorSlice.reducer;
